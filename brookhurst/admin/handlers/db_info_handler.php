ad6106b18f2e6d75143f275e72e5f0dc58189132
Allan Jeremy <aj.dev254@gmail.com>
2016-07-14 15:25:10 +0300

    Added Schedule section base for admin -to allow teachers to schedule classes - added `class_id` and `stream_id` to schedules table (todo in table templates)

--- brookhurst/admin/handlers/db_info_handler.php
+++ brookhurst/admin/handlers/db_info_handler.php
@@ -50,4 +50,76 @@ function getAvailableClasses()
 		return false;
 	}	
 }
+
+function getAvailableSchedules($teacher_id)
+{
+	require("../esomoDbConnect.php");
+	$q = "SELECT * FROM schedules WHERE teacher_id=$teacher_id";
+
+	if($result = mysqli_query($dbCon,$q))
+	{	
+		if(mysqli_num_rows($result)>0)
+		{
+			return $result;
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	else
+	{
+		return false;
+	}
+}
+
+function getClassName($class_id)
+{
+	{
+	require("../esomoDbConnect.php");
+	$q = "SELECT class_name FROM class_selection WHERE class_id=$class_id";
+
+	if($result = mysqli_query($dbCon,$q))
+	{	
+		if(mysqli_num_rows($result)>0)
+		{	
+			#return the first classname under the id provided
+			foreach($result as $r)
+			{return $r;}
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	else
+	{
+		return false;
+	}
+}
+
+function getStreamName($stream_id)
+{
+	{
+	require("../esomoDbConnect.php");
+	$q = "SELECT stream_name FROM streams WHERE stream_id=$stream_id";
+
+	if($result = mysqli_query($dbCon,$q))
+	{	
+		if(mysqli_num_rows($result)>0)
+		{	
+			#return the first classname under the id provided
+			foreach($result as $r)
+			{return $r;}
+		}
+		else
+		{
+			return 0;
+		}
+	}
+	else
+	{
+		return false;
+	}
+}
 }#end of class
