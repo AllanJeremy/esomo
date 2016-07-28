01d5608f65f56f7fc91df405683d4c597805fe77
mwauramuchiri <gramwauu@gmail.com>
2016-07-28 14:07:20 +0300

    correction

--- brookhurst/admin/index.php
+++ brookhurst/admin/index.php
@@ -31,7 +31,7 @@ session_start();
 $curUsername = $_SESSION['s_admin_username'];
 $indexContent = <<<EOD
       
-       <div class='container-fluid'>
+       
 
         <div class='col-sm-3'>
           <h3>Admin Panel</h3>
