f7bc0685777750d232033e87b2f182819d672082
Allan Jeremy <aj.dev254@gmail.com>
2016-07-28 12:05:39 +0300

    Added add to respective database table upload
    - files are uploaded and the records are inserted into the appropriate database tables when this happens
    
    TODO:
    - Add file type restriction for upload as function parameter for `uploadFile` and subsequent functions
    
    - Add file size restriction for upload as function parameter for `uploadFile` and subsequent functions

--- brookhurst/admin/handlers/submitContent.php
+++ brookhurst/admin/handlers/submitContent.php
@@ -12,38 +12,105 @@ $fileUpload->uploadContent('adm_contFileInput');
 #topicDropdown - topic dropdown
 #subjectDropdown - subject dropwdown
 #contTypeDropdown - content type dropdown
+$adm_contTitleInput = htmlspecialchars(@$_POST['adm_contTitleInput']);
 
 switch($_POST['contTypeDropdown'])
 {
     case 'article':
-    addArticle();
+    addArticle($fileUpload->storage_path);
     break;
 
     case 'book':
-    addBook();
+    addBook($fileUpload->storage_path);
     break;
 
     case 'video':
-    addVideo();
+    addVideo($fileUpload->storage_path);
     break;
 
     default:
 }
 
 #add an article to the database
-function addArticle()
+function addArticle($storagePath)
 {
-    echo '<br><br>added an article to the database';
+    $q = "INSERT INTO esomo_articles(article_path,article_title,topic_id,thumbnail_path) VALUES(?,?,?,?)";
+    $thumbnailPath = '';#path to the thumbnail
+
+    if($stmt = $GLOBALS['dbCon']->prepare($q))
+    {
+        // echo '<br>added an article to the database';
+        // echo "<br><b>File storage path</b> = ".$storagePath;
+        
+        $stmt->bind_param('ssis',$storagePath,$GLOBALS['adm_contTitleInput'],$_POST['topicDropdown'],$thumbnailPath);
+
+        #Check if the query executed successfully
+        if($stmt->execute())
+        {
+            echo "<br>Added article to database";
+            #redirect user
+        }
+        else #query failed to run, display error message
+        {
+            echo "failed to execute the query to add article<br>";
+        }
+    }
+    else #failed to prepare the query
+     {
+        echo "<p>Error preparing query to add article</p>";
+    }
 }
 
 #add a book to the database
-function addBook()
+function addBook($storagePath)
 {
-  echo '<br><br>added a book to the database';
+    $q = "INSERT INTO esomo_books(book_path,book_title,topic_id,thumbnail_path) VALUES(?,?,?,?)";
+    $thumbnailPath = '';#path to the thumbnail
+
+    if($stmt = $GLOBALS['dbCon']->prepare($q))
+    {
+        $stmt->bind_param('ssis',$storagePath,$GLOBALS['adm_contTitleInput'],$_POST['topicDropdown'],$thumbnailPath);
+
+        #Check if the query executed successfully
+        if($stmt->execute())
+        {
+            echo "<br>Added book to database";
+            #redirect user
+        }
+        else #query failed to run, display error message
+        {
+            echo "failed to execute the query to add book<br>";
+        }
+    }
+    else #failed to prepare the query
+     {
+        echo "<p>Error preparing query to add book</p>";
+    }
 }
 
 #add a video to the database
-function addVideo()
+function addVideo($storagePath)
 {
- echo '<br><br>added a video to the database';
+    $q = "INSERT INTO esomo_videos(video_path,video_title,topic_id,thumbnail_path) VALUES(?,?,?,?)";
+    $thumbnailPath = '';#path to the thumbnail
+
+    if($stmt = $GLOBALS['dbCon']->prepare($q))
+    {
+        $stmt->bind_param('ssis',$storagePath,$GLOBALS['adm_contTitleInput'],$_POST['topicDropdown'],$thumbnailPath);
+
+        #Check if the query executed successfully
+        if($stmt->execute())
+        {
+            echo "<br>Added video to database";
+            #redirect user
+        }
+        else #query failed to run, display error message
+        {
+            echo "failed to execute the query to add video<br>";
+        }
+    }
+    else #failed to prepare the query
+     {
+        echo "<p>Error preparing query to add video</p>";
+    }
 }
