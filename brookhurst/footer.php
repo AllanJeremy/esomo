c0ed3708a1f1c2d53f459483af300962512c3052
mwauramuchiri <gramwauu@gmail.com>
2016-09-29 13:14:22 +0300

    added sticky footer-> add <main> tag after <body> tag

--- brookhurst/footer.php
+++ brookhurst/footer.php
@@ -1,10 +1,9 @@
 <!--footer for the site-->
-<div class="container-fluid clearfix footer">
-<footer >
+</main>
+<footer  class="container-fluid clearfix footer" >
     <p> ©  <?php echo "".date("Y").""?> Brookhurst International School </p>
 
     <!--Developer website :  http://www.deflix.com -->
     <sub class='float_right dark_link'>Developed by <a href="#" target="blank">Deflix Enterprises</a></sub>
     <br>
 </footer>
-</div>
